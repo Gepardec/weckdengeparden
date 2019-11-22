5976cdc07ab8fa978aa1388968bece1b432a7775
Philipp Wurm <philipp.wurm@gepardec.com>
2019-11-22 09:17:37 +0100

    initial commit

---
+++ src/main/java/com/gepardec/wdg/Application.java
@@ -0,0 +1,16 @@
+package com.gepardec.wdg;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Readiness
+@ApplicationScoped
+public class Application implements HealthCheck {
+
+    public HealthCheckResponse call () {
+        return HealthCheckResponse.up("WDG Application is ready!");
+    }
+}
