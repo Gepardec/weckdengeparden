b7348640698299b0aa3ecfdd04159304249844e7
Thomas <thomas.herzog@gepardec.com>
2020-02-04 15:15:10 +0100

    - Returning 500 for personio exceptions only if they are not explicitly handled - deleted duplicate http request in test.http

--- src/main/java/com/gepardec/wdg/challenge/exception/PersonioClientExceptionMapper.java
+++ src/main/java/com/gepardec/wdg/challenge/exception/PersonioClientExceptionMapper.java
@@ -3,6 +3,7 @@ package com.gepardec.wdg.challenge.exception;
 import com.gepardec.wdg.application.exception.ExceptionHandledEvent;
 import com.gepardec.wdg.challenge.model.BaseResponse;
 import com.gepardec.wdg.client.personio.PersonioError;
+import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -34,7 +35,9 @@ public class PersonioClientExceptionMapper implements ExceptionMapper<PersonioCl
     @Override
     public Response toResponse(PersonioClientException exception) {
         log.error(String.format("Call on resource '%s' failed because personio rest call failed", uriInfo.getPath()), exception);
-        final Response response = Response.serverError().entity(BaseResponse.error(exception.applicationError.clientMessage)).build();
+        final Response response = Response.status(getHttpResponseStatusForPersonioError(exception.applicationError))
+                .entity(BaseResponse.error(exception.applicationError.clientMessage))
+                .build();
         handledEvent.fire(ExceptionHandledEvent.Builder.newBuilder(exception)
                 .withMessage(String.format("Response-Status: %d, Response-Message: %s, Personio-Error: %s, Client-Response: %s",
                         exception.status,
@@ -44,5 +47,13 @@ public class PersonioClientExceptionMapper implements ExceptionMapper<PersonioCl
                 .withIsError(true)
                 .build());
         return response;
+    }
+
+    private int getHttpResponseStatusForPersonioError(final PersonioError personioError) {
+        if (PersonioError.UNDEFINED.equals(personioError)) {
+            return HttpStatus.SC_INTERNAL_SERVER_ERROR;
+        }
+
+        return HttpStatus.SC_BAD_REQUEST;
     }
 }
