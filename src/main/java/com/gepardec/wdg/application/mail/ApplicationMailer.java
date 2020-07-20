eac8dc5016a4db0cd9f75fcfe382d277f649f660
Philipp <philipp.zelinger@gepardec.com>
2020-07-20 11:22:06 +0200

    Add defaultmailaddressmethod

--- src/main/java/com/gepardec/wdg/application/mail/ApplicationMailer.java
+++ src/main/java/com/gepardec/wdg/application/mail/ApplicationMailer.java
@@ -3,16 +3,24 @@ package com.gepardec.wdg.application.mail;
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
 import io.quarkus.mailer.reactive.ReactiveMailer;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.inject.Inject;
 
 public class ApplicationMailer {
+
+    @ConfigProperty(name = "ApplicationMailer.Default")
+    private String defaultMailAddress;
 
     @Inject
     private Mailer mailer;
 
     @Inject
     private ReactiveMailer reactiveMailer;
+
+    public void sendMailToDefaultMailAddress(String subject, String text) {
+        mailer.send(Mail.withText(defaultMailAddress, subject, text));
+    }
 
     public void sendMail(String receiver, String subject, String text) {
         mailer.send(Mail.withText(receiver, subject, text));
