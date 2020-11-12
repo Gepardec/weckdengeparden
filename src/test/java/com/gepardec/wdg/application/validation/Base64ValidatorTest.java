2971dff604418e7c5ecc4a4622dd7d2f5537449c
agnes <agnes.hinterplattner@gmx.at>
2020-11-12 20:24:02 +0100

    Base64Validator --> REGEX für BASE64_PATTERN verändert sodass 1 Test fehlschlägt

--- src/test/java/com/gepardec/wdg/application/validation/Base64ValidatorTest.java
+++ src/test/java/com/gepardec/wdg/application/validation/Base64ValidatorTest.java
@@ -37,7 +37,7 @@ class Base64ValidatorTest {
         final String base64 = createBase64StringForPadding(0);
         Assertions.assertTrue(validator.isValid(base64, null));
     }
-
+    // Test will fail because of a broken CodePart
     @Test
     void isValid_withPadding1_thenTruealse() {
         final String base64 = createBase64StringForPadding(1);
