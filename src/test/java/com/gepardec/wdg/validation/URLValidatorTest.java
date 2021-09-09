4f4f27e392f819c75ff575e829addc229c801d8c
Relasym <thomas.plasser@gmail.com>
2021-09-09 11:40:18 +0200

    project unbroken

--- src/test/java/com/gepardec/wdg/validation/URLValidatorTest.java
+++ src/test/java/com/gepardec/wdg/validation/URLValidatorTest.java
@@ -19,11 +19,48 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class URLValidatorTest {
 
+    @Mock
+    private ConstraintValidatorContext.ConstraintViolationBuilder constraintViolationBuilder;
+
+    @Mock
+    private ConstraintValidatorContext.ConstraintViolationBuilder.NodeBuilderCustomizableContext nodeBuilderCustomizableContext;
+
+    @Mock
+    private ConstraintValidatorContext context;
+
+    private URLValidator validator;
+
+    @BeforeEach
+    void beforeEach() {
+        validator = new URLValidator();
+    }
+
     //@WINStage3: Irgendwie ein komischer Test für den URL-Validator, findest Du nicht auch?
     @Test
-    void musterTest() {
-        final int x = 5;
-        Assertions.assertTrue(x == 7);
+    void isValid_withValidSource_thenTrue() {
+        //Assertions.assertTrue(x == 5); //passt doch ;)
+        AnswerChallenge2 answer = new AnswerChallenge2();
+        answer.setUrl("https://github.com/Gepardec/weckdengeparden/pull/12345");
+        Assertions.assertTrue(validator.isValid(answer,context));
+    }
+
+    @Test
+    void isValid_withNullSource_thenFalse() {
+        Assertions.assertFalse(validator.isValid(null,context));
+    }
+
+    @Test
+    void isValid_withEmptySource_thenFalse() {
+        Assertions.assertFalse(validator.isValid(new AnswerChallenge2(),context));
+    }
+
+    @Test
+    void isValid_withInvalidSource_thenFalse() {
+        when(constraintViolationBuilder.addPropertyNode(anyString())).thenReturn(nodeBuilderCustomizableContext);
+        when(context.buildConstraintViolationWithTemplate(anyString())).thenReturn(constraintViolationBuilder);
+        AnswerChallenge2 answer = new AnswerChallenge2();
+        answer.setUrl("https://github.com/Leopardec/weckdenleoparden/pull/12345");
+        Assertions.assertFalse(validator.isValid(answer,context));
     }
 
 }
